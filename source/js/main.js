59954789ca68be8c75473c0f2ce9ddc712ebae7e
Alyona <alenka.inform@gmail.com>
2022-12-10 15:39:31 +0300

    Add block gym

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,6 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
+import {videoEnabled} from './modules/project/video-active';
 
 // ---------------------------------
 
@@ -16,6 +17,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+    videoEnabled();
     initModals();
   });
 });
