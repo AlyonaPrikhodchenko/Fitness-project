23be0eef1aa5b01790c5d1f557093581a351c9c9
Alyona <alenka.inform@gmail.com>
2022-12-12 10:10:09 +0300

    Add block trainers

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,3 +1,38 @@
 // Swiper 7.4.1
 // import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
+import './vendor/swiper';
+
+// eslint-disable-next-line
+new Swiper('#swiperTrainer', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    280: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 22,
+    },
+
+    320: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 22,
+    },
+
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 30,
+    },
+
+    1200: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 40,
+    },
+  },
+});
